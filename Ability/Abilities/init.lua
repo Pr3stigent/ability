77ad663cfe007819dff99081f68e30bcc2d27538
Pr3stigent <83563230+Pr3stigent@users.noreply.github.com>
2021-11-15 16:14:05 -0400

    Delete Ability/Abilities directory

--- Ability/Abilities/init.lua
+++
@@ -1,9 +0,0 @@
-local ModuleScripts = {}
-
-for _, moduleScript in ipairs(script:GetChildren()) do
-	if moduleScript:IsA("ModuleScript") then
-		ModuleScripts[moduleScript.Name] = require(moduleScript)
-	end
-end
-
-return ModuleScripts
